dabbf7a5d678688d0e5cb1b39c245ff7b558f43a
LidiaO <lidiacorazon1914@hotmail.com>
2014-06-26 15:07:14 -0500

    Update Conexion.java

--- ProyectoOL/src/pe/upeu/edu/proyectooolanol/config/Conexion.java
+++ ProyectoOL/src/pe/upeu/edu/proyectooolanol/config/Conexion.java
@@ -12,5 +12,23 @@ package pe.upeu.edu.proyectooolanol.config;
  * @author alum.fial8
  */
 public class Conexion {
+    private static Connection connex = null;
+    private static final String url="jdbc:mysql//localhost/bd_visitas";
+    private static final String usuario="root";
+    private static final String clave="root";
     
+    public static Connection getConexion(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            if(connex == null){
+                connex = DriverManager.getConection(url, usuario, clave);
+            }else{
+                connex = null;
+            }
+        }catch(ClassNotFoundException | SQLException e){
+            JOptionPane.ShowMessageDialog(null, e);
+        }
+        return conex;
+    }
+
 }
